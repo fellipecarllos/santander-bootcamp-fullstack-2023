4c33e5d4958254c7c6b8e242dfbf2aa0dd67c559
Felipe Carlos <fellipecarllos@msn.com>
2023-10-20 16:08:03 -0300

    first commit

---
+++ src/test/java/com/dio/santanderbootcampfullstack2023/SantanderBootcampFullstack2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santanderbootcampfullstack2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampFullstack2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
